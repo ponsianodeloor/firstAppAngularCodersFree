af64320530dd99980edc89236c2a9125d27e25ae
Ponsiano De Loor <ponsianodeloor@gmail.com>
2023-10-07 14:47:57 -0500

    crear propiedades, objetos y objetos a partir de interfaces

--- src/app/contador/contador/contador.component.ts
+++ src/app/contador/contador/contador.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Person } from '../../interfaces/person';
 
 @Component({
   selector: 'app-contador',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./contador.component.css']
 })
 export class ContadorComponent {
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  name:string = 'Contador App';
+
+  person = {
+    name: 'Fernando',
+    age: 35,
+  }
+
+  secondPerson:Person = {
+    name: 'Melissa',
+    age: 34,
+  }
 
 }
